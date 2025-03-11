6d3e7deb1d4f0f220d6bac4278a904c4348ee6cc
yzdmusic <118027024+yzdmusic@users.noreply.github.com>
2025-03-11 18:01:37 +0700

    hai

--- search-engine-allpages.js
+++ search-engine-allpages.js
@@ -74,6 +74,7 @@ const data = [
     { name: "Rise Above", url: "album-rise-above.html", image: "https://yzdmusic.github.io/cover-1/RISE ABOVE (COVER).jpg", price: "Album" },
     { name: "Rowdiest", url: "album-rowdiest.html", image: "https://yzdmusic.github.io/cover-1/ROWDIEST (COVER).jpg", price: "Album" },
     { name: "Sekte Sesat", url: "album-sekte-sesat.html", image: "https://yzdmusic.github.io/cover-1/SEKTE SESAT (COVER).jpg", price: "Album" },
+    { name: "Serenity And Harmony", url: "album-serenity-and-harmony.html", image: "https://yzdmusic.github.io/cover-1/SERENITY AND HARMONY (COVER).jpg", price: "Album" },
     { name: "Serenity", url: "album-serenity.html", image: "https://yzdmusic.github.io/cover-1/SERENITY (COVER).jpg", price: "Album" },
     { name: "Silent Room", url: "album-silent-room.html", image: "https://yzdmusic.github.io/cover-1/SILENT ROOM (COVER).jpg", price: "Album" },
     { name: "Simfoniyyet El 'Ishq", url: "album-simfoniyyet-el-ishq.html", image: "https://yzdmusic.github.io/cover-1/SIMFONIYYET EL 'ISHQ (COVER).jpg", price: "Album" },

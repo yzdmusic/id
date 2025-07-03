e43ca0e5c992725ee79fa569be181adb83b1217f
yzdmusic <118027024+yzdmusic@users.noreply.github.com>
2025-07-03 20:44:08 +0700

    js

--- js/collapsible-store.js
+++ js/collapsible-store.js
@@ -44,6 +44,52 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+document.addEventListener("DOMContentLoaded", function () {
+
+    var menuItems = document.querySelectorAll('.name-categories');
+    var defaultContent = document.querySelector('#albums');
+    var defaultMenu = document.querySelector('.name-categories a[href="#albums"]').parentElement;
+
+    if (defaultContent) {
+        defaultContent.style.display = 'block';
+        defaultMenu.classList.add('active');
+    }
+
+    // Tambahkan event listener untuk setiap elemen menu
+    menuItems.forEach(function (menuItem) {
+        menuItem.addEventListener('click', function (event) {
+            // Mencegah link default agar tidak melakukan navigasi
+            event.preventDefault();
+
+            // Dapatkan href dari menu yang diklik
+            var targetId = this.querySelector('a').getAttribute('href');
+
+            // Cari elemen dengan ID yang sesuai
+            var targetContent = document.querySelector(targetId);
+
+            // Sembunyikan semua elemen card
+            var allContents = document.querySelectorAll('.card-content');
+            allContents.forEach(function (content) {
+                content.style.display = 'none';
+            });
+
+            // Hapus kelas 'active' dari semua menu nav
+            menuItems.forEach(function (item) {
+                item.classList.remove('active');
+            });
+
+            // Tampilkan elemen card yang sesuai
+            if (targetContent) {
+                targetContent.style.display = 'block';
+                // Tambahkan kelas 'active' ke menu yang diklik
+                this.classList.add('active');
+            }
+        });
+    });
+});
+
+
+
 
 
 // GAYA FONT
